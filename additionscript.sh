9f6855b7a1bfeb2376425f90cb0823caf443d5c5
Vaishnavi-buradkar <Vaishnaviburadkar@gmail.com>
2024-08-10 18:05:27 +0530

    first commit of adition script

---
+++ additionscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+
+#############
+#Author: vaishnavi
+#Date:- 10th Aug
+
+#addittion of 2 numbers script
+#############
+
+n=4
+m=5
+a=$((n + m))
+echo "addition of $n + $m is $a"
+
