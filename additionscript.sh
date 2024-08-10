b7f40fe1a199e29dd0854af4048c5071603ecf70
Vaishnavi-buradkar <Vaishnaviburadkar@gmail.com>
2024-08-10 18:39:54 +0530

    added new feature to add 3 numbers

--- additionscript.sh
+++ additionscript.sh
@@ -9,12 +9,23 @@ set -x
 #addittion of 2 numbers script
 #############
 
-echo -n "Enter first number: "
+#echo -n "Enter first number: "
+#read num1
+
+#echo -n "Enter 2nd number: "
+#read num2
+
+#sum=$((num1 + num2))
+#echo "addition of $num1 + $num2 is $sum"
+
+# New Feature for sum of 3 numbers
+
+echo -n "Enter three numbers: "
 read num1
-
-echo -n "Enter 2nd number: "
 read num2
+read num3
 
-sum=$((num1 + num2))
-echo "addition of $num1 + $num2 is $sum"
+sum=$((num1 + num2 + num3))
+echo "addition of $num1 + $num2 + $num3 is $sum"
+
 
