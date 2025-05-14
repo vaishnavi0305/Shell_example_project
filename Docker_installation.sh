0df0500017dac07f72daeb473a8e0b6d61a7e842
Ubuntu <ubuntu@ip-172-31-83-201.ec2.internal>
2025-05-14 09:22:04 +0000

    Written script to install docker

---
+++ Docker_installation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#update the package index and install required dependences
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add the current user to the 'docker' group to run Docker commands without sudo
+sudo usermod -aG docker $USER
+
+#start and enable docker service
+sudo systemctl start docker
+sudo systemctl enable docker
+
+#print docker version
+docker --version
